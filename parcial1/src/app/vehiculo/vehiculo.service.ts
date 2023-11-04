656236b0a31b243a16245480161b12024e41aa63
Nelson <n.ortizf@uniandes.edu.co>
2023-11-04 16:26:48 -0500

    Actualizacion clase servicio y url en archivo environment

--- parcial1/src/app/vehiculo/vehiculo.service.ts
+++ parcial1/src/app/vehiculo/vehiculo.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Vehiculo } from './vehiculo';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class VehiculoService {
 
+  private apiUrl = environment.baseUrl + '202212_MISW4104_Grupo1.json';
+
 constructor(private http: HttpClient) { }
+
+getVehiculos(): Observable<Vehiculo[]>{
+  return this.http.get<Vehiculo[]>(this.apiUrl);
+}
 
 }
