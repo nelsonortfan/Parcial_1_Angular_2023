da18b55b22bb66b81e0b06df72cfec1ded91e50c
Nelson <n.ortizf@uniandes.edu.co>
2023-11-05 09:57:44 -0500

    Creacion 3 vehiculos y validacion de tabla para pruebas

--- parcial1/src/app/vehiculo/vehiculo-list/vehiculo-list.component.spec.ts
+++ parcial1/src/app/vehiculo/vehiculo-list/vehiculo-list.component.spec.ts
@@ -2,18 +2,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { VehiculoListComponent } from './vehiculo-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { Vehiculo } from '../vehiculo';
+import { VehiculoService } from '../vehiculo.service';
 
 describe('VehiculoListComponent', () => {
   let component: VehiculoListComponent;
   let fixture: ComponentFixture<VehiculoListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule],
-      declarations: [ VehiculoListComponent ]
+      declarations: [ VehiculoListComponent ],
+      providers: [VehiculoService]
     })
     .compileComponents();
   }));
@@ -21,10 +26,26 @@ describe('VehiculoListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(VehiculoListComponent);
     component = fixture.componentInstance;
+
+    for(let i = 0; i < 3; i++){
+      const vehiculo = new Vehiculo(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+      );
+      component.vehiculos.push(vehiculo);
+    }
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Componente tiene una tabla',()=>{
+    expect(debug.query(By.css("tbody")).childNodes.length).toBeGreaterThan(0);
+  });
+
 });
