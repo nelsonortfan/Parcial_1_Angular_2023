c17ff25843c03c18e875f0b950647733c958844c
Nelson <n.ortizf@uniandes.edu.co>
2023-11-04 21:12:16 -0500

    Inicio conteo de marcas

---
+++ parcial1/src/app/vehiculo/marcas.ts
@@ -0,0 +1,19 @@
+export class Marcas {
+
+  marca: string;
+  cantidad: number;
+
+  public constructor(marca:string, cantidad:number){
+    this.marca = marca;
+    this.cantidad = cantidad;
+  }
+
+  public incrementarCantidad(){
+    this.cantidad = this.cantidad + 1;
+  }
+
+  public getMarca(){
+    return this.marca;
+  }
+
+}
--- parcial1/src/app/vehiculo/vehiculo-list/vehiculo-list.component.ts
+++ parcial1/src/app/vehiculo/vehiculo-list/vehiculo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { VehiculoService } from '../vehiculo.service';
 import { Vehiculo } from '../vehiculo';
+import { Marcas } from '../marcas';
 
 @Component({
   selector: 'app-vehiculo-list',
@@ -12,13 +13,38 @@ export class VehiculoListComponent implements OnInit {
   constructor(private vehiculoService: VehiculoService) { }
 
   vehiculos: Array<Vehiculo> = []
+  marcas: Array<Marcas> = []
+  longitud1: number = 2;
+  longitud: number = 2;
+  num: number = 0
+  numMarcas: number = 0;
 
   getVehiculos(){
     this.vehiculoService.getVehiculos().subscribe(vehiculos =>{
       this.vehiculos = vehiculos;
+      this.longitud = this.vehiculos.length;
+      this.getAgrupacionMarcas();
     });
   }
 
+  getAgrupacionMarcas(){
+    for(this.num = 0; this.num< this.longitud; this.num++){
+      if(this.num == 0){
+        this.marcas[0] = new Marcas(this.vehiculos[0].marca,1);
+      }
+      else {
+        this.numMarcas = this.numMarcas + 1;
+        this.marcas.forEach(element => {
+          if(element.getMarca() == this.vehiculos[this.num].marca){
+            this.marcas[this.num].incrementarCantidad();
+          }
+          else{
+            this.marcas[this.numMarcas] = new Marcas(this.vehiculos[this.num].marca,1);
+          }
+        });
+      }
+    }
+  }
 
 
   ngOnInit() {
